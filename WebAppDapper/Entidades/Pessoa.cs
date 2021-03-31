184090b0eb169fdfc7c7c6ab4fcb7017f0100b0a
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-31 17:01:37 -0300

    Criação da entidade Pessoa

---
+++ WebAppDapper/Entidades/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppDapper.Entidades
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string GenderId { get; set; }
+    }
+}
