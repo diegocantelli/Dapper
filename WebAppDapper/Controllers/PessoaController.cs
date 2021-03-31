eeb9c1564b01ccdc21dd68400351b169c4575ee8
Diego Cantelli <diegocantelli19@gmail.com>
2021-03-31 17:02:02 -0300

    Executando query com Dapper

---
+++ WebAppDapper/Controllers/PessoaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebAppDapper.Entidades;
+
+namespace WebAppDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public PessoaController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pessoa>> GetPessoas()
+        {
+            using (SqlConnection connection = new SqlConnection(
+                _config.GetConnectionString("ExemplosDapper")))
+            {
+                //connection.Query -> Query é o método de extensão do DAPPER
+                var query = connection.Query<Pessoa>($" SELECT Id, Name, Email, GenderId " +
+                    $"FROM tblPerson ");
+
+                return Ok(query);
+            }
+        }
+    }
+}
