afb902946bcc8eba672ea5643ebef68f2f0cc4c2
Diego Cantelli <diegocantelli19@gmail.com>
2021-04-01 14:43:07 -0300

    Adicionando uma referência da classe gênero na classe Pessoa

--- WebAppDapper/Entidades/Pessoa.cs
+++ WebAppDapper/Entidades/Pessoa.cs
@@ -11,5 +11,6 @@ namespace WebAppDapper.Entidades
         public string Name { get; set; }
         public string Email { get; set; }
         public string GenderId { get; set; }
+        public Genero Genero { get; set; }
     }
 }
